d6d55ca406f181be33813b2a6bff7521d40d9f8d
Victoria Benoit <vrose834@gmail.com>
2023-03-28 10:49:26 -0300

    push

--- Develop/public/assets/js/routers/htmlRouter.js
+++ Develop/public/assets/js/routers/htmlRouter.js
@@ -1,13 +1,15 @@
 const htmlRouter = require('express').Router();
+const path = require('path');
+
+// Get the index.html file:
+htmlRouter.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../Develoop/public/index.html'));
+})
 
 //Should return the notes.html file
 htmlRouter.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+    res.sendFile(path.join(__dirname, '../Develop/public/notes.html'));
 })
 
-// Get the index.html file:
-htmlRouter.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
-})
 
 module.exports = htmlRouter;
