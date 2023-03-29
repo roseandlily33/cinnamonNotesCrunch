5b58d48f3c5c6d74f6d1f92204f3a679f3424013
Victoria Benoit <vrose834@gmail.com>
2023-03-29 12:59:36 -0300

    push

--- Develop/routers/apiRouter.js
+++ Develop/routers/apiRouter.js
@@ -20,19 +20,22 @@ apiRouter.get('/notes', (req, res) => {
 apiRouter.post('/notes', (req, res) => {
     console.log(`${req.method} is made`);
     console.log('New Note Recieved' + req.body);
-
+    
     let newNote = req.body;
     newNote.id = uuidv4();
-
+    //newNote = JSON.stringify(newNote); giving [object Object]
+    
     readNotes('Develop/db/db.json', 'utf-8')
         .then(data => {
-            const parseNotes = JSON.parse(data);
-            parseNotes.push(newNote);
+            console.log(data);
+            const parsedNotes = JSON.parse(data);
+            console.log(typeof newNote);
+            console.log(parsedNotes);
+            parsedNotes.push(newNote);
             console.log('Parsed and pushed');
-            writeNotes('Develop/db/db.json', JSON.stringify(parseNotes));
+            writeNotes('Develop/db/db.json', JSON.stringify(newNote));
         })
-
-
+        .catch(err => console.log(err));
 })
 //Recieve a query paramter that contains the id of a note to delete, it reads all notes from the db.json file, and removes the note with the given id property and then rewrites the  notes to the db.json file. 
 apiRouter.delete('/api/notes/:id', (req, res) => {
