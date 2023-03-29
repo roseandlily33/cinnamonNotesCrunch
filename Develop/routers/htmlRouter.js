c62a351c4097b4d99c3d5bcb98df1e117ce2525c
Victoria Benoit <vrose834@gmail.com>
2023-03-29 08:51:05 -0300

    couple api.router paths

--- Develop/routers/htmlRouter.js
+++ Develop/routers/htmlRouter.js
@@ -3,10 +3,12 @@ const path = require('path');
 
 //Should return the notes.html file
 htmlRouter.get('/notes', (req, res) => {
+    console.log('Routed to notes')
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 })
 // Get the index.html file:
-htmlRouter.get('/*', (req, res) => {
+htmlRouter.get('/', (req, res) => {
+    console.log('Routed to main')
     res.sendFile(path.join(__dirname, '../public/index.html'));
 })
 
